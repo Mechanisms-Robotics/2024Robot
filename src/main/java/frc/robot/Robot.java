85243eedfb4b6620ac4da5340357fc71337b28c2
Micah Maphet <micahmaphet@gmail.com>
2024-02-01 19:16:08 -0500

    fixed intake implementation deletion

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -65,7 +65,6 @@ public class Robot extends TimedRobot {
   @Override
   public void testInit() {
     CommandScheduler.getInstance().cancelAll();
-    m_robotContainer.intake.zero();
   }
 
   @Override
