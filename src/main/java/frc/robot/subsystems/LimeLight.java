140f0575468ca2947e0c174aa1210aeb5e2c9adc
Micah Maphet <micahmaphet@gmail.com>
2024-03-19 11:33:53 -0400

    added code to get the position of the robot from multiple april tags

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -1,21 +1,34 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import java.util.ArrayList;
+
 public class LimeLight extends SubsystemBase {
     private final PhotonCamera camera = new PhotonCamera("LimeLight");
+    // TODO: get the transform 3d of the camera to the robot
+    private final Transform3d cameraToRobot = new Transform3d(0, 0, 0, new Rotation3d());
     private int aprilTagID = 7;
     private double yaw = 0;
     private double area = 0;
+    /** The transform of the field to the camera */
     private Transform3d fieldToCamera = new Transform3d();
+    private ArrayList<Pose3d> robotPositions = new ArrayList<>();
+    private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+
 
     /**
      * Data of the LimeLight
@@ -23,6 +36,10 @@ public class LimeLight extends SubsystemBase {
      * @param yaw angle made by the center of the camera and the target
      * @param area % area that the target takes up on the camera, used for distance
      * @param hasTarget true if the camera sees an AprilTag otherwise false
+     * @param aimed true if the camera yaw is centered with the target within tolerance
+     * @param robotPositions ArrayList of Pose3ds calculated by each detected april tag
+     * @param fieldToCamera Transform3d of the field to the camera based on multiple april tags
+     * @param aprilTagFieldLayout layout of all april tags on the field
      */
 
     public record LimeLightData(
@@ -30,7 +47,9 @@ public class LimeLight extends SubsystemBase {
             double area,
             boolean hasTarget,
             boolean aimed,
-            Transform3d fieldToCamera
+            ArrayList<Pose3d> robotPositions,
+            Transform3d fieldToCamera,
+            AprilTagFieldLayout aprilTagFieldLayout
     ) {}
 
     /**
@@ -40,8 +59,8 @@ public class LimeLight extends SubsystemBase {
      * @return data of the LimeLight: yaw, area, and hasTarget
      */
     public LimeLightData getData() {
-        return new LimeLightData(yaw, area, area!=0,
-                                 MathUtil.isNear(0, yaw, 5), fieldToCamera);
+        return new LimeLightData(yaw, area, area!=0, MathUtil.isNear(0, yaw, 5),
+                robotPositions, fieldToCamera, aprilTagFieldLayout);
     }
 
     @Override
@@ -65,14 +84,27 @@ public class LimeLight extends SubsystemBase {
 
         boolean hasTarget = result.hasTargets();
         SmartDashboard.putBoolean("[Lime Light] Has Target", hasTarget);
+
+        robotPositions.clear();
         // if any of the targets is the correct AprilTag, set the yaw and area
         // otherwise set them to 0
         for (PhotonTrackedTarget target : result.getTargets()) {
+            // get the position of the robot based on the target
+            if (aprilTagFieldLayout.getTagPose(target.getFiducialId()).isPresent()) {
+                Transform3d bestCameraToTarget = target.getBestCameraToTarget();
+                Pose3d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(
+                        target.getBestCameraToTarget(),
+                        aprilTagFieldLayout.getTagPose(target.getFiducialId()).get(),
+                        cameraToRobot);
+
+                robotPositions.add(robotPose);
+            }
             if (target.getFiducialId() == aprilTagID) {
                 yaw = target.getYaw();
                 area = target.getArea();
                 return;
             }
+
         }
         yaw = 0;
         area = 0;
