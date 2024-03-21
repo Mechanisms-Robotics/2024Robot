e6a49bf16f7377ec2882d76b0918505833cb053f
Micah Maphet <micahmaphet@gmail.com>
2024-03-21 19:24:48 -0400

    added data collection for the amp AprilTag

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -23,6 +23,8 @@ public class LimeLight extends SubsystemBase {
     private final Transform3d cameraToRobot = new Transform3d(0, 0, 0, new Rotation3d());
     /** Creates the AprilTag data holder for the subwoofer */
     private final Tag subTag = new Tag(7, 4);
+    /** Create the AprilTag data holder fot he amp */
+    private final Tag ampTag = new Tag(6, 5);
     /** The transform of the field to the camera */
     private Transform3d fieldToCamera = new Transform3d();
     private ArrayList<Pose3d> robotPositions = new ArrayList<>();
@@ -39,6 +41,7 @@ public class LimeLight extends SubsystemBase {
 
     public record LimeLightData(
             Tag subTag,
+            Tag ampTag,
             ArrayList<Pose3d> robotPositions,
             Transform3d fieldToCamera,
             AprilTagFieldLayout aprilTagFieldLayout
@@ -48,15 +51,44 @@ public class LimeLight extends SubsystemBase {
         int aprilTagID;
         double yaw;
         double area;
-        public Tag(int blueID, int redID, double yaw, double area) {
+        boolean lastUpdated = false;
+        /**
+         * Construct the AprilTag IDs for the blue and the red alliance.
+         * The AprilTagID is automatically set based on the alliance it is on.
+         * The tag IDs are shown on the
+         * <a href="https://firstfrc.blob.core.windows.net/frc2024/Manual/2024GameManual.pdf">Crescendo Game Manual</a>
+         * on page 35.
+         */
+        public Tag(int blueID, int redID) {
             if (DriverStation.getAlliance().get() == Alliance.Blue) aprilTagID = blueID;
             else if (DriverStation.getAlliance().get() == Alliance.Red) aprilTagID = redID;
             else throw new RuntimeException("The alliance was not found and AprilTag ID culd no be set cuh");
-            this.yaw = yaw;
-            this.area = area;
+            this.yaw = 0;
+            this.area = 0;
         }
-        public Tag(int blueID, int redID) {
-            this(blueID, redID, 0, 0);
+
+        /**
+         * If the target is this AprilTags target (has the same ID), update the yaw and area.
+         * Does nothing if it is not the right AprilTag
+         *
+         * @param target AprilTag
+         */
+        public void update(PhotonTrackedTarget target) {
+            if (target.getFiducialId() == aprilTagID) {
+                yaw = target.getYaw();
+                area = target.getArea();
+                lastUpdated = true;
+            }
+        }
+
+        /**
+         * Clear the data of the AprilTag.
+         * Should be done if no data was found on the last cycle
+         */
+        public void clear() {
+            yaw = 0;
+            area = 0;
+            lastUpdated = false;
         }
         public record data(
                 int aprilTagID,
@@ -77,7 +109,7 @@ public class LimeLight extends SubsystemBase {
      * @return data of the LimeLight: yaw, area, and hasTarget
      */
     public LimeLightData getData() {
-        return new LimeLightData(subTag, robotPositions, fieldToCamera, aprilTagFieldLayout);
+        return new LimeLightData(subTag, ampTag, robotPositions, fieldToCamera, aprilTagFieldLayout);
     }
 
     @Override
@@ -85,6 +117,10 @@ public class LimeLight extends SubsystemBase {
         SmartDashboard.putNumber("[Lime Light] [Sub] Yaw", subTag.yaw);
         SmartDashboard.putNumber("[Lime Light] [Sub] Area", subTag.area);
         SmartDashboard.putNumber("[Lime Light] [Sub]", subTag.aprilTagID);
+
+        SmartDashboard.putNumber("[Lime Light] [Amp] Yaw", ampTag.yaw);
+        SmartDashboard.putNumber("[Lime Light] [Amp] Area", ampTag.area);
+        SmartDashboard.putNumber("[Lime Light] [Amp]", ampTag.aprilTagID);
 
         // get the data from the camera
         PhotonPipelineResult result = camera.getLatestResult();
@@ -109,13 +145,12 @@ public class LimeLight extends SubsystemBase {
 
                 robotPositions.add(robotPose);
             }
-            if (target.getFiducialId() == subTag.aprilTagID) {
-                subTag.yaw = target.getYaw();
-                subTag.area = target.getArea();
-                return;
-            }
+            // update AprilTag data
+            subTag.update(target);
+            ampTag.update(target);
         }
-        subTag.yaw = 0;
-        subTag.area = 0;
+        // if there was data for any of the tags AprilTag found on the cycle, clear that tags data
+        if (subTag.lastUpdated) subTag.clear();
+        if (ampTag.lastUpdated) ampTag.clear();
     }
 }
