da41f35d52cdcf918ca77cce5319f932170827c9
Micah Maphet <micahmaphet@gmail.com>
2024-03-21 18:52:11 -0400

    refactored the code to put the subwoofer april tag data in a class in LimeLight

--- src/main/java/frc/robot/subsystems/LimeLight.java
+++ src/main/java/frc/robot/subsystems/LimeLight.java
@@ -21,9 +21,8 @@ public class LimeLight extends SubsystemBase {
     private final PhotonCamera camera = new PhotonCamera("LimeLight");
     // TODO: get the transform 3d of the camera to the robot
     private final Transform3d cameraToRobot = new Transform3d(0, 0, 0, new Rotation3d());
-    private int aprilTagID = 7;
-    private double yaw = 0;
-    private double area = 0;
+    /** Creates the AprilTag data holder for the subwoofer */
+    private final Tag subTag = new Tag(7, 4);
     /** The transform of the field to the camera */
     private Transform3d fieldToCamera = new Transform3d();
     private ArrayList<Pose3d> robotPositions = new ArrayList<>();
@@ -33,24 +32,43 @@ public class LimeLight extends SubsystemBase {
     /**
      * Data of the LimeLight
      *
-     * @param yaw angle made by the center of the camera and the target
-     * @param area % area that the target takes up on the camera, used for distance
-     * @param hasTarget true if the camera sees an AprilTag otherwise false
-     * @param aimed true if the camera yaw is centered with the target within tolerance
      * @param robotPositions ArrayList of Pose3ds calculated by each detected april tag
      * @param fieldToCamera Transform3d of the field to the camera based on multiple april tags
      * @param aprilTagFieldLayout layout of all april tags on the field
      */
 
     public record LimeLightData(
-            double yaw,
-            double area,
-            boolean hasTarget,
-            boolean aimed,
+            Tag subTag,
             ArrayList<Pose3d> robotPositions,
             Transform3d fieldToCamera,
             AprilTagFieldLayout aprilTagFieldLayout
     ) {}
+
+    public class Tag {
+        int aprilTagID;
+        double yaw;
+        double area;
+        public Tag(int blueID, int redID, double yaw, double area) {
+            if (DriverStation.getAlliance().get() == Alliance.Blue) aprilTagID = blueID;
+            else if (DriverStation.getAlliance().get() == Alliance.Red) aprilTagID = redID;
+            else throw new RuntimeException("The alliance was not found and AprilTag ID culd no be set cuh");
+            this.yaw = yaw;
+            this.area = area;
+        }
+        public Tag(int blueID, int redID) {
+            this(blueID, redID, 0, 0);
+        }
+        public record data(
+                int aprilTagID,
+                double yaw,
+                double area,
+                boolean hasTarget,
+                boolean aimed
+        ) {}
+        public data getData() {
+            return new data(aprilTagID, yaw, area, area!=0, MathUtil.isNear(0, yaw, 5));
+        }
+    }
 
     /**
      * Returns object LimeLightData with all data of the LimeLight.
@@ -59,22 +77,14 @@ public class LimeLight extends SubsystemBase {
      * @return data of the LimeLight: yaw, area, and hasTarget
      */
     public LimeLightData getData() {
-        return new LimeLightData(yaw, area, area!=0, MathUtil.isNear(0, yaw, 5),
-                robotPositions, fieldToCamera, aprilTagFieldLayout);
+        return new LimeLightData(subTag, robotPositions, fieldToCamera, aprilTagFieldLayout);
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("[Lime Light] Target Yaw", yaw);
-        SmartDashboard.putNumber("[Lime Light] Target Area", area);
-        // If the Alliance is Blue, look for AprilTag 7, otherwise (when it is red) look for AprilTag 4
-        // If there is no alliance specified, print error
-        if (DriverStation.getAlliance().isPresent()) {
-            if (DriverStation.getAlliance().get() == Alliance.Blue) aprilTagID = 7;
-            else aprilTagID = 4;
-        } else { System.out.println("The Alliance was no found"); }
-        SmartDashboard.putNumber("[Lime Light] Target ID", aprilTagID);
-
+        SmartDashboard.putNumber("[Lime Light] [Sub] Yaw", subTag.yaw);
+        SmartDashboard.putNumber("[Lime Light] [Sub] Area", subTag.area);
+        SmartDashboard.putNumber("[Lime Light] [Sub]", subTag.aprilTagID);
 
         // get the data from the camera
         PhotonPipelineResult result = camera.getLatestResult();
@@ -99,14 +109,13 @@ public class LimeLight extends SubsystemBase {
 
                 robotPositions.add(robotPose);
             }
-            if (target.getFiducialId() == aprilTagID) {
-                yaw = target.getYaw();
-                area = target.getArea();
+            if (target.getFiducialId() == subTag.aprilTagID) {
+                subTag.yaw = target.getYaw();
+                subTag.area = target.getArea();
                 return;
             }
-
         }
-        yaw = 0;
-        area = 0;
+        subTag.yaw = 0;
+        subTag.area = 0;
     }
 }
